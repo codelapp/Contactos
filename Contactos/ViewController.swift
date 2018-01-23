4b544172bfa7851d41c6bd8105a2737c6b9c16ed
Jorge Maldonado Borbón <jorge@2806-108e-0005-c6b5-10d2-57e6-a233-f961.ipv6.infinitum.net.mx>
2018-01-23 16:38:38 -0600

    Initial Commit

---
+++ Contactos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contactos
+//
+//  Created by Jorge Maldonado Borbón on 23/01/18.
+//  Copyright © 2018 Jorge Maldonado Borbón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
