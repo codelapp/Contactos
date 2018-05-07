888d8284b654e68141e30dd0fa4ec3fc26f15882
codelapp <info@codelapp.com>
2018-05-07 11:15:04 +0200

    Commit final

---
+++ Contactos/Contactos.swift
@@ -0,0 +1,15 @@
+//
+//  Contactos.swift
+//  Contactos
+//
+//  Created by Jorge Maldonado Borbón on 23/01/18.
+//  Copyright © 2018 Jorge Maldonado Borbón. All rights reserved.
+//
+
+import Foundation
+
+struct Contactos {
+    let nombre : String
+    let apellido : String
+    let numero : String
+}
--- Contactos/ViewController.swift
+++ Contactos/ViewController.swift
@@ -1,25 +1,96 @@
-//
-//  ViewController.swift
-//  Contactos
-//
-//  Created by Jorge Maldonado Borbón on 23/01/18.
-//  Copyright © 2018 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
-
-class ViewController: UIViewController {
-
+import Contacts
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    
+    @IBOutlet weak var tabla: UITableView!
+    var listaContactos = [Contactos]()
+    var contactosStore = CNContactStore()
+    @IBOutlet weak var editarOutlet: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tabla.delegate = self
+        tabla.dataSource = self
+        
+        contactosStore.requestAccess(for: .contacts) { (aceptar, error) in
+            if aceptar {
+                print("acceso permitido")
+            }else{
+                print("acceso denegado")
+            }
+            
+            if let error = error {
+                print("hubo un error en los contactos", error)
+            }
+        }
+        fetchContactos()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return listaContactos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tabla.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let contacto = listaContactos[indexPath.row]
+        cell.textLabel?.text = "\(contacto.nombre) \(contacto.apellido)"
+        cell.detailTextLabel?.text = contacto.numero
+        return cell
+    }
+    
+    func fetchContactos(){
+        let key = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+        let request = CNContactFetchRequest(keysToFetch: key)
+        request.sortOrder = CNContactSortOrder.givenName
+        try! contactosStore.enumerateContacts(with: request, usingBlock: { (contacto, point) in
+            let nombre = contacto.givenName
+            let apellido = contacto.familyName
+            let numero = contacto.phoneNumbers.first?.value.stringValue ?? "sin numero"
+            let contactosAgregar = Contactos(nombre: nombre, apellido: apellido, numero: numero)
+            self.listaContactos.append(contactosAgregar)
+        })
+        DispatchQueue.main.async {
+            self.tabla.reloadData()
+        }
     }
 
-
+    @IBAction func editar(_ sender: UIBarButtonItem) {
+        tabla.isEditing = !tabla.isEditing
+        if tabla.isEditing {
+            editarOutlet.title = "Aceptar"
+        }else{
+            editarOutlet.title = "Editar"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = listaContactos[sourceIndexPath.row]
+        listaContactos.remove(at: sourceIndexPath.row)
+        listaContactos.insert(item, at: sourceIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            listaContactos.remove(at: indexPath.row)
+            DispatchQueue.main.async {
+                self.tabla.reloadData()
+            }
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
 
